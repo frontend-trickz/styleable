176e5a0906afd3b76ae27aa0697f6693bad23f21
Steve S. Samson <stevee.samson@gmail.com>
2022-09-25 22:59:55 +0100

    move to frontend-trickz

---
+++ docs/_app/immutable/chunks/1-22e6b0a5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-2643a762.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 	kit: {
 		adapter: adapter({ pages: 'docs', assets: 'docs' }),
 		paths: {
-			base: dev ? '' : '/svelte-styleable'
+			base: dev ? '' : '/styleable'
 		}
 		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
 		// For example, instead of '_app', use 'app_', 'internal', etc.
